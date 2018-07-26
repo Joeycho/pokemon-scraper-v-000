56f6df4997e74c8c0bcc6d41c4e7f177a81aa9b3
Joey Cho <cjy12182000@naver.com>
2018-07-26 09:21:46 +0000

    automatically backed up by learn

--- spec/pokemon_spec.rb
+++ spec/pokemon_spec.rb
@@ -44,10 +44,8 @@ describe "Pokemon" do
 
     before do
       @sql_runner.execute_create_hp_column
-      binding.pry
       Pokemon.save('Pikachu', 'electric', @db)
       Pokemon.save('Magikarp', 'water', @db)
-      binding.pry
     end
 
     let(:pikachu){Pokemon.find(1, @db)}
